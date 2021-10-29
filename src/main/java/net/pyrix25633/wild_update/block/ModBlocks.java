b67137ae8c8a920e5376b73d9784df6d812852b3
Pyrix25633 <biral.mattia@gmail.com>
2021-10-29 18:26:35 +0200

    Mud Brick Slab and Stairs

--- src/main/java/net/pyrix25633/wild_update/block/ModBlocks.java
+++ src/main/java/net/pyrix25633/wild_update/block/ModBlocks.java
@@ -5,6 +5,7 @@ import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
+import net.minecraft.block.SlabBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
@@ -12,27 +13,39 @@ import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.pyrix25633.wild_update.WildUpdate;
+import net.pyrix25633.wild_update.block.custom.ModStairsBlock;
 
 public class ModBlocks {
 
+    // Mud Blocks
     public static final Block MUD_BLOCK = registerBlock("mud_block",
             new Block(FabricBlockSettings.of(Material.SOLID_ORGANIC)
                     .strength(0.85f, 1f)
                     .jumpVelocityMultiplier(0.9f)
                     .breakByTool(FabricToolTags.SHOVELS)
                     .sounds(BlockSoundGroup.ROOTED_DIRT)));
-
     public static final Block MUD_BRICKS = registerBlock("mud_bricks",
             new Block(FabricBlockSettings.of(Material.SOLID_ORGANIC)
                     .strength(1.5f, 2f)
                     .breakByTool(FabricToolTags.PICKAXES).requiresTool()
                     .sounds(BlockSoundGroup.STONE)));
+    public static final Block MUD_BRICK_STAIRS = registerBlock("mud_brick_stairs",
+            new ModStairsBlock(ModBlocks.MUD_BRICKS.getDefaultState(),
+                    FabricBlockSettings.of(Material.SOLID_ORGANIC)
+                    .strength(1.5f, 2f)
+                    .breakByTool(FabricToolTags.PICKAXES).requiresTool()
+                    .sounds(BlockSoundGroup.STONE)));
+    public static final Block MUD_BRICK_SLAB = registerBlock("mud_brick_slab",
+            new SlabBlock(FabricBlockSettings.of(Material.SOLID_ORGANIC)
+                    .strength(1.5f, 2f)
+                    .breakByTool(FabricToolTags.PICKAXES).requiresTool()
+                    .sounds(BlockSoundGroup.STONE)));
 
-    public static final Block MANGROVE_LOG = registerBlock("mangrove_log",
-            new Block(FabricBlockSettings.of(Material.WOOD)
-                    .strength(1f, 1f)
-                    .breakByTool(FabricToolTags.AXES)
-                    .sounds(BlockSoundGroup.WOOD))); //Model and texture!
+    //public static final Block MANGROVE_LOG = registerBlock("mangrove_log",
+    //        new Block(FabricBlockSettings.of(Material.WOOD)
+    //                .strength(1f, 1f)
+    //                .breakByTool(FabricToolTags.AXES)
+    //                .sounds(BlockSoundGroup.WOOD))); //Model and texture!
 
     private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
